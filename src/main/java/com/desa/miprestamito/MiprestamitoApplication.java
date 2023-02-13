90aed6c2ff645a0fe0565350dcaa0e97a41247a8
Juarezgump655 <ajuarezcastellanos45@gmail.com>
2023-02-12 21:14:34 -0600

    first commit

---
+++ src/main/java/com/desa/miprestamito/MiprestamitoApplication.java
@@ -0,0 +1,13 @@
+package com.desa.miprestamito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprestamitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiprestamitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desa/miprestamito/MiprestamitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desa.miprestamito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiprestamitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
